46401726da9d97cfdeb8416b00fca266c29bf0a0
Raul Neto <raulneto90@gmail.com>
2020-12-11 15:59:49 -0300

    Create repository detail page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +12,54 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/30750063?s=460&u=9566feeda9a9321c000c55953e38f3bb212fb1a4&v=4"
+            alt="Raul Neto"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasds" key="{repository.full_name}">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,158 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+
+    transition: color 0.2s;
+
+    &:hover {
+      color: #666;
+    }
+  }
+
+  svg {
+    margin-right: 4px;
+    font-size: 20px;
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+`;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 16px;
+  }
+
+  svg {
+    margin-left: 15px;
+    cursor: pointer;
+    font-size: 20px;
+    color: #c53030;
+  }
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(5px);
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbc8d6;
+      font-size: 20px;
+    }
+  }
+
+  @media (max-width: 425px) {
+    & {
+      img {
+        width: 48px;
+        height: 48px;
+      }
+
+      div {
+        strong {
+          font-size: 17px;
+        }
+        p {
+          font-size: 15px;
+        }
+      }
+
+      svg {
+        font-size: 48px;
+      }
+    }
+  }
+`;
