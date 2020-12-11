d3e1f05eb063c80d2e350f4be3540d0c66a73d3e
Raul Neto <raulneto90@gmail.com>
2020-12-11 09:53:17 -0300

    Add styles to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/30750063?s=460&u=9566feeda9a9321c000c55953e38f3bb212fb1a4&v=4"
+            alt="Raul Neto"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Unform is a performance focused library that helps you creating
+              beautiful forms in ReactJS
+            </p>
+          </div>
+          <FiChevronRight />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/30750063?s=460&u=9566feeda9a9321c000c55953e38f3bb212fb1a4&v=4"
+            alt="Raul Neto"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Unform is a performance focused library that helps you creating
+              beautiful forms in ReactJS
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,163 @@
+import { shade } from 'polished';
 import styled from 'styled-components';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+
+  margin-top: 80px;
+
+  line-height: 56px;
+  max-width: 450px;
+
+  @media (max-width: 425px) {
+    & {
+      font-size: 42px;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  @media (max-width: 425px) {
+    & {
+      flex-direction: column;
+
+      input {
+        width: 100%;
+      }
+
+      button {
+        margin-top: 8px;
+        width: 100%;
+        border-radius: 5px;
+      }
+    }
+  }
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+
+  @media (max-width: 425px) {
+    & {
+      flex-direction: column;
+
+      input {
+        width: 100%;
+        border-radius: 5px;
+        flex: inherit;
+      }
+
+      button {
+        margin-top: 8px;
+        width: 100%;
+        border-radius: 5px;
+      }
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbc8d6;
+      font-size: 20px;
+    }
+
+    @media (max-width: 425px) {
+      & {
+        img {
+          width: 48px;
+          height: 48px;
+        }
+
+        div {
+          strong {
+            font-size: 17px;
+          }
+          p {
+            font-size: 15px;
+          }
+        }
+
+        svg {
+          font-size: 48px;
+        }
+      }
+    }
+  }
 `;
